e381236279337a9a839f681ca6ae76921245c6f1
yasuanke <ihshinron1@gmail.com>
2018-05-13 23:23:23 -0600

    Terminado la estructura HTML

--- src/components/Address/index.js
+++ src/components/Address/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
-import img from '../../assets/imperial-beach.jpg';
+import imperialBeach from '../../assets/imperial-beach.jpg';
 
 const Address = () => (
-  <section>
-    
+  <section id="Address" className="container">
+    <img src={imperialBeach} alt="imperialBeach"/>
+    <article className="wrapAddress">
+      <small>Our Camp</small>
+      <h2 className="title">CA 91932, USA Imperial Beach 560 Silver Strand Blvd</h2>
+      <a href="">Get in touch</a>
+    </article>
   </section>
 )
 
